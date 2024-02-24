23b897206f83fb410364ced0306d2edf7f6f1bb4
munaimun <munaimunz@gmail.com>
2024-02-24 17:26:37 +0600

    worked on logo and navbar

--- src/Component/Navbar.js
+++ src/Component/Navbar.js
@@ -1,27 +1,33 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import "./Navbar.css"
+import Logo from '../assets/F.png'
 
 const Navbar = () => {
   return (
     <div className='header'>
 
-      <div className='logo'>
+    <div id='title'>
+      <Link to="/" className='logo-img'>
+          <img src={Logo} alt='Logo' className='logo-img' />
+      </Link>
+      <div className='title-name'>
         <Link to="/">Descending</Link>
       </div>
-
-      <div id='nav-links'>
-        <Link className='nav-link' to="/">Home</Link>
-        <Link className='nav-link' to="/about">About Us</Link>
-        <Link className='nav-link active' to="/service">Services</Link>
-        <Link className='nav-link' to="/contact">Contact</Link>
-        <span className='span'></span>
-      </div>
-
-        <div className='products-link'>
-          <a href='https://view-finder-nine.vercel.app/' target='blank'>Our Prodects</a>
-        </div>
     </div>
+
+    <div id='nav-links'>
+      <Link className='nav-link' to="/">Home</Link>
+      <Link className='nav-link' to="/about">About Us</Link>
+      <Link className='nav-link active' to="/service">Services</Link>
+      <Link className='nav-link' to="/contact">Contact</Link>
+      <span className='span'></span>
+    </div>
+
+    <div className='products-link'>
+      <a href='https://view-finder-nine.vercel.app/' target='_blank' rel='noopener noreferrer'>Our Products</a>
+    </div>
+  </div>
   )
 }
 
